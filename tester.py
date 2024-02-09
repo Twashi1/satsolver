e81b488cb64a557e54626e67312d69627db86011
twashi1 <thomas.zarri@durham.ac.uk>
2024-02-09 14:17:36 +0000

    a

---
+++ tester.py
@@ -0,0 +1,5 @@
+# TODO: need some extended DIMACS format, ideally can hold multiple formulae in one file,
+#   and store all satisfying truth assignments for testing
+#       or at least whether or not the formula is satisfiable
+
+# TODO: need to convert extended DIMACS format into a regular DIMACS so it can be fed to whatever sat solver
